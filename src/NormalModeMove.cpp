3aec6e411a7bbfdc678496af837fdf6748464a35
trashh <egor.perri2006@gmail.com>
2025-07-25 02:24:13 +0300

    added NormalModeMove class

---
+++ src/NormalModeMove.cpp
@@ -0,0 +1,36 @@
+//
+// Created by pentester on 7/25/25.
+//
+
+
+#include "NormalModeMove.h"
+
+#include <algorithm>
+#include <numeric>
+
+bool NormalModeMove::check_move(int n, int a, int b, const std::vector<bool> &field, const std::vector<int> &sticks) {
+    if (sticks.size() > 3 || sticks.size() < 1) {
+        return false;
+    }
+
+    for (int i: sticks) {
+        if (i >= n || !field[i]) {
+            return false;
+        }
+    }
+    if (sticks.size() == 3) {
+        std::sort(sticks.begin(), sticks.end());
+        if (sticks[0] + 1 != sticks[1] || sticks[1] != sticks[2]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool NormalModeMove::is_over(int n, int a, int b, const std::vector<bool> &field) {
+    int sum = std::std::accumulate(field.begin(), field.end(), 0);
+    if (sum < 1) {
+        return true;
+    }
+    return false;
+}
